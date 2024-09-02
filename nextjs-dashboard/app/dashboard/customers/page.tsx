3dcfb688f2c6383afcc2674768caaf52ab86e471
ulugbek989898 <47236469+ulugbek989898@users.noreply.github.com>
2024-09-02 11:07:03 +0400

    Add files via upload
    first commit

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function CustomerPage() {
+    return (
+        <div>
+            <h1>Customers</h1>
+            <p>Here are all the customers</p>
+        </div>
+    )
+}
---
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function InvoicesPage() {
+    return (
+        <div>
+            <h1>Invoices</h1>
+            <p>Here are all the invoices</p>
+        </div>
+    )
+}
